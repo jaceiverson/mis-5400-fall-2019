2aa0d1d7b5e3aa4f0a82a308fecff1dd7fedae1a
mckellyp <mckelly@thepearts.com>
2019-09-11 14:58:43 -0600

    mod_2 and mod_3

---
+++ mod_1/scratch.py
@@ -0,0 +1,3 @@
+print('Hello MIS 5400 Fall 2019')
+
+input
---
+++ mod_3/mod_3_hmwk.py
@@ -0,0 +1,45 @@
+######################################################
+#   MIS 5400
+#   Module 3 Homework
+#
+#   INSTRUCTIONS
+#   1) Write code to to complete exercises below.
+#   2) Save the file and submit it using Canvas.
+######################################################
+'''
+MIS 5400 Module 3 Homework
+'''
+###############
+# Exercise  1 #
+###############
+'''
+    Write code to analyze each number between 2000 and 6500 and do the following (HINT use the range function)
+     1.) If the number is divisible by 5 then print out [Fitty].
+     2.) If the number is divisible by 7 then print out [Sevvy].
+     3.) If the number is divisible by BOTH 5 and 7 print out ["Winner's win", said Bob] (quotes included)
+'''
+
+# WRITE YOUR CODE HERE
+
+
+
+###############
+# Exercise  2 #
+###############
+'''
+    Using the file "access.log", write code that does the following:
+    
+    1) Read the file into a list.
+    2) Print out the following information:
+
+    How many Total logs are there? 
+    How many logs have a status code of 404? (Hint: Membership Checking)
+    How many logs have a status code of 200?
+    How many of the logs contain the text "mis"?
+
+    3) Write some code that replaces all instances of "redflag" with "greenlight" (Hint: string replace method)
+    4) Put all logs with the replaced values in a new list
+    5) Create a file named "mis5400.log" and write out the list with the replaced values. 
+'''
+
+# WRITE YOUR CODE HERE
